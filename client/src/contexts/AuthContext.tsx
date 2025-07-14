c8567710f02e9f59669489643d8b63119e3040b4
faham112 <fkjaan7@gmail.com>
2025-07-14 17:36:39 +0500

    Added useAuth hook to AuthContext

--- client/src/contexts/AuthContext.tsx
+++ client/src/contexts/AuthContext.tsx
@@ -1,5 +1,10 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 
 const AuthContext = createContext(null);
+
+// ✅ Yeh hook bana rahe hain jo baad mein import hoga
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
 
 export default AuthContext;
